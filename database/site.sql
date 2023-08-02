313d029e650e688e261a210bc4ed5e56cef43043
kuldeep <hytek112@gmail.com>
2023-08-03 02:52:04 +0530

    new schema of db

--- database/site.sql
+++ database/site.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 27, 2023 at 10:27 PM
+-- Generation Time: Aug 02, 2023 at 11:21 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -33,44 +33,18 @@ CREATE TABLE `blog` (
   `short_desc` varchar(255) NOT NULL,
   `date` date NOT NULL DEFAULT current_timestamp(),
   `img` varchar(255) NOT NULL,
-  `slug` varchar(255) NOT NULL
+  `slug` varchar(255) NOT NULL,
+  `content` mediumtext NOT NULL,
+  `author` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `blog`
 --
 
-INSERT INTO `blog` (`id`, `title`, `short_desc`, `date`, `img`, `slug`) VALUES
-(1, 'This Is my First Blog', 'Here You See mY ! st Blog Post', '2023-07-28', 'assets/img/blog/blog-1.jpg', 'test-test'),
-(2, 'sdfdgfdgfdrgrfed', 'dfgfdgfdgfdg', '2023-07-28', 'assets/img/blog/blog-2.jpg', 'sdffgdf'),
-(3, 'cxvbvcfb', 'cdfghfghbgf', '2023-07-28', 'assets/img/blog/blog-1.jpg', 'fgfd-sdf'),
-(4, 'cxvbvcbnbvnbvfnbf', 'fdgbfghtfrggbtggtrhgt', '2023-07-28', 'assets/img/blog/blog-1.jpg', 'asdgfsjdsdjhf');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `blogcontent`
---
-
-CREATE TABLE `blogcontent` (
-  `id` int(255) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `slug` varchar(255) NOT NULL,
-  `date` date NOT NULL DEFAULT current_timestamp(),
-  `author` varchar(255) NOT NULL,
-  `content` text NOT NULL,
-  `img` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `blogcontent`
---
-
-INSERT INTO `blogcontent` (`id`, `title`, `slug`, `date`, `author`, `content`, `img`) VALUES
-(1, 'dfgggfggftdgfdgbfgbgfbgfbfg', 'test-test', '2023-07-28', 'chandan', '<h1 class=\"bg-primary\">hello world </h1>\r\nsfdfnbsdfjnb\r\nsfsdfnsdbvsd sdf\r\nsfdff nbsdf', 'assets/img/blog/blog-1.jpg'),
-(2, 'fdfdgfd', 'fgfd-sdf', '2023-07-28', 'aaspas', 'dfvfdfdvgfdvfdv  cdfvfd', 'assets/img/blog/blog-1.jpg'),
-(3, 'dfgfdgbdfgvb', 'sdffgdf', '2023-07-28', 'dfgfdg', 'dfggfhggfg', 'assets/img/blog/blog-2.jpg'),
-(4, 'dfgfdgfdg', 'asdgfsjdsdjhf', '2023-07-28', 'fsdgtrght', 'dtghtgrhbfdgbgfrh', 'assets/img/blog/blog-1.jpg');
+INSERT INTO `blog` (`id`, `title`, `short_desc`, `date`, `img`, `slug`, `content`, `author`) VALUES
+(8, 'dfg', 'fghgf', '2023-08-03', '_38c3e1ee-4510-4182-a3bc-d6004c7060e7.jpeg', 'fghfg', 'fghfhj', 'kuldeepit'),
+(9, 'cvbvcbn', 'cvcbncvbn', '2023-08-03', '_6e1c5af2-2674-426a-b482-8e845cddb866.jpeg', 'ccgvbcgvfb', 'cvbc', 'kuldeepit');
 
 -- --------------------------------------------------------
 
@@ -97,7 +71,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `uname`, `fname`, `lname`, `phone_no`, `email`, `passwd`, `date`, `gender`, `avatar_img`, `dob`) VALUES
-(1, '', '', '', '', 'k4kuldeep108@gmail.com', '$2y$10$1mSax43I9kAUc7BZ4jZC4uAcNEqeUOghVolAgcvqMpF7xtIW.rXIi', '2023-07-27 23:41:06.00000', '', '', '0000-00-00');
+(1, 'kuldeep', '', '', '', 'k4kuldeep108@gmail.com', '$2y$10$1mSax43I9kAUc7BZ4jZC4uAcNEqeUOghVolAgcvqMpF7xtIW.rXIi', '2023-07-27 23:41:06.00000', '', '', '0000-00-00'),
+(4, 'kuldeepit', '', '', '', 'kuldeep@hytek.org.in', '$2y$10$OygP.imKkow9LhX/qw1jLe.ZmKjUcTHHUw6bd0nF/z2dgDK.6McYm', '2023-08-03 01:08:40.00000', '', '', '0000-00-00');
 
 --
 -- Indexes for dumped tables
@@ -107,13 +82,6 @@ INSERT INTO `users` (`id`, `uname`, `fname`, `lname`, `phone_no`, `email`, `pass
 -- Indexes for table `blog`
 --
 ALTER TABLE `blog`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `slug` (`slug`);
-
---
--- Indexes for table `blogcontent`
---
-ALTER TABLE `blogcontent`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `slug` (`slug`);
 
@@ -131,19 +99,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `blog`
 --
 ALTER TABLE `blog`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `blogcontent`
---
-ALTER TABLE `blogcontent`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
